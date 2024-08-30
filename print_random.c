46cec3df0026dc0f0004314e89b874f7225a4baf
andrewkelley-1 <tuq39505@temple.edu>
2024-08-30 12:54:57 -0400

    Updated print_random.c to the latest announcement

--- print_random.c
+++ print_random.c
@@ -1,26 +1,20 @@
-//Given Project code
-#include "random.h"
 #include <stdio.h>
 #include <stdlib.h>
-char* rand_string_alloc(size_t size);
-char* rand_string(char *s,size_t size);
+#include <time.h>
 
-char* rand_string_alloc(size_t size)
+
+
+int main()
 {
-     char *s = malloc(size + 1);
-     if (s) {
-         rand_string(s, size);
-     }
-     return s;
-}
-char* rand_string(char *s,size_t size){
-    for(int i=0; i<size;i++){
-        s[i]=randchar();
-    }
-    s[size]='\0';
-    return s;
-}
-int main(){
-    rand_string_alloc(5);
-    return 0;
+	int a;
+
+	/* seed the randomizer */
+	srand( (unsigned)time(NULL) );
+
+	printf("Today's random word: ");
+	for(a=0;a<7;a++)
+		putchar( randchar() );
+	putchar('\n');
+
+	return(0);
 }
